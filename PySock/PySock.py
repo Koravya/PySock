7e645ed256888b0adac0f28d844ee13320e01873
Brian Williams <brimw234@outlook.com>
2021-03-18 16:04:40 -0500

    added Client Class

--- PySock/PySock.py
+++ PySock/PySock.py
@@ -1,5 +1,7 @@
 import socket
 import json
+from queue import Queue
+from threading import Thread
 
 class commModule:
     def __init__(self, ip, port=None):
@@ -35,3 +37,78 @@ class commModule:
 
     IP = property(lambda self: self._IP, lambda self, ip: self._setIP(ip))
     PORT = property(lambda self: self._Port, lambda self, port: self._setPort(port))
+
+class commClient(commModule):
+    def __init__(self, ip, port=None):
+        super().__init__(ip, port)
+        self._Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._MSGSPan = 1024
+
+        self._QIN = Queue()
+        self._QOut = Queue()
+        self._Daemon = None
+
+    def connect(self, remoteIP, remotePort):
+        self._Daemon = Thread(target=self._worker)
+        self._Sock.connect((remoteIP, remotePort))
+    
+    def disConnect(self):
+        self._QOut.put('EXIT')
+        self._Daemon.join()
+        self._Daemon = None
+
+        self._Sock.close()
+
+    def _serialize(self, message):
+        return bytes(super()._serialize(message))
+
+    def _deserialize(self, message):
+        return super()._deserialize(message.decode('utf-8'))
+
+    def sendMessage(self, message, messageType):
+        self._Sock.sendall(self._serialize(message))
+
+    def hasPending(self):
+        return not self._QIN.empty()
+
+    def reciveMessage(self):
+        if not self._QIN.empty():
+            msg = self._QIN.get_nowait()
+
+            if isinstance(msg, Exception):
+                raise msg
+            
+            return self._deserialize(msg)
+
+    def _worker(self):
+        exe = True
+        while exe:
+            try:
+                if not self._QOut.empty():
+                    msg = self._QOut.get_nowait()
+
+                    if msg == 'EXIT':
+                        exe = False
+                    else:
+                        self._Sock.sendall(msg)
+            
+                msg = ''
+
+                while True:
+                    data = self._Sock.recv(self._MSGSPan)
+
+                    if len(data) <= 0:
+                        break
+                    else:
+                        msg += data
+
+                self._QIN.put(msg)
+            
+            except Exception as e:
+                self._QIN.put(e)
+
+
+
+            
+
+
