d84ab02f84ee9b65f0fd8de28eee912a207a4d10
Brian Williams <brimw234@outlook.com>
2021-03-18 13:32:38 -0500

    Fixed set Property of IP and Port objects

--- PySock/PySock.py
+++ PySock/PySock.py
@@ -6,6 +6,12 @@ class commModule:
         self._IP = ip.split(':')[0] if ':' in ip else ip
         self._Port = port if not port else ip.split(':')[1] if ':' in ip else None
 
+    def _setIP(self, ip):
+        self._IP = ip
+
+    def _setPort(self, port):
+        self._Port = port
+
     def _serialize(self, message):
         return json.dumps(message)
 
@@ -27,5 +33,5 @@ class commModule:
     def reciveMessage(self, timeout=0):
         pass
 
-IP = property(lambda self: self._IP, lambda self, ip: self._IP = ip)
-PORT = property(lambda self: self._Port, lambda self, port: self._Port = port)
+IP = property(lambda self: self._IP, lambda self, ip: self._setIP(ip))
+PORT = property(lambda self: self._Port, lambda self, port: self._setPort(port))
